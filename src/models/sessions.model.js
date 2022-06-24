31f08b2bdba332911b1a3bbd8f1d024bc6ac2423
Nisarg <falcon.nisarg@gmail.com>
2022-06-24 18:43:57 +0530

    Session User Id Associations

--- src/models/sessions.model.js
+++ src/models/sessions.model.js
@@ -1,6 +1,11 @@
+
+
 module.exports = (mongoose) => {
     const sessionSchema = new mongoose.Schema({
-        user_id: String,
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'users'
+        },
         token: String
     }, {
         timestamps: true
